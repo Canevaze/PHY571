541a232d68c9890c4e566698010ba5858cb9b476
Clément Brunel <126831841+brunelclement@users.noreply.github.com>
2023-11-28 18:10:06 +0100

    new file to plot fig3

--- Plot fig 3.py
+++ Plot fig 3.py
@@ -4,15 +4,18 @@ import itertools
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import FuncAnimation
-from tqdm import tqdm
+#from tqdm import tqdm
 
 from Class import *
         
             
 #create swarm of birds and do an animation of the evolution
 
-v = np.load('name_file_v.npy')
-eta = np.load('name_file_eta.npy')
+#v = np.load('https://github.com/Canevaze/PHY571/blob/21a57986093a7471c2b700f9360e4145c796fa35/v_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
+#eta = np.load('https://github.com/Canevaze/PHY571/blob/21a57986093a7471c2b700f9360e4145c796fa35/eta_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
+
+v= np.load('v_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
+eta = np.load('eta_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,8 +31,12 @@ def plot_v_vs_eta_subplot(v, eta, eta_critical_values):
         x_values = (eta_critical - eta) / eta_critical
 
         # Plot v in function of (eta_critical - eta) / eta_critical in each subplot
-        axes[i].plot(x_values, v, label=f'eta_critical = {eta_critical}')
+        axes[i].scatter(x_values, v, label=f'eta_critical = {eta_critical}')
         
+        axes[i].set_ylim(0.1, 1)
+        axes[i].set_yscale('log')
+        axes[i].set_xlim(0.01, 1)
+        axes[i].set_xscale('log')
         # Add labels and title to the first subplot
         if i == 0:
             axes[i].set_xlabel(f'({eta_critical} - eta) / {eta_critical}')
@@ -38,6 +45,8 @@ def plot_v_vs_eta_subplot(v, eta, eta_critical_values):
 
         # Add a legend to each subplot
         axes[i].legend()
+        axes[i].grid(True)
+
 
     # Adjust layout
     plt.tight_layout()
@@ -47,7 +56,7 @@ def plot_v_vs_eta_subplot(v, eta, eta_critical_values):
 
 # Example usage:
 # Generate some example data
-eta_critical_values = [2, 3, 4, 5]
+eta_critical_values = [4,4]
 
 # Call the function to plot the data with different eta_critical values
 plot_v_vs_eta_subplot(v, eta, eta_critical_values)
