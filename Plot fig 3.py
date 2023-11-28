aa8c259a7515ebdab8de178edc851f085da953de
Nicolas Cazenave <nicolas.cazenave@polytechnique.edu>
2023-11-28 15:38:05 +0100

    New model and new figure

---
+++ Plot fig 3.py
@@ -0,0 +1,45 @@
+import numpy as np
+import numpy.random as rnd
+import itertools
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation
+from tqdm import tqdm
+
+from Class import *
+        
+            
+#create swarm of birds and do an animation of the evolution
+
+
+def retrieve_param_eta(eta,it,N,V, L, R):
+    swarm = Swarm(L, N, V, eta, R)
+    swarm.initialize()
+
+    for i in range(it):
+        swarm.evolve()
+    
+    return swarm.get_swarm_mean_velocity()
+
+def plot(it=200,N=40,V=0.03, L=3.1, R=1):
+    eta = np.linspace(0.0,5,50)
+    v = []
+    for e in tqdm(eta):
+        average = []
+        for i in range(10):
+            average.append(retrieve_param_eta(e,it,N,V, L, R))
+        v.append(np.mean(average))
+
+
+
+    #plot it all
+
+    plt.scatter(eta,v)
+    plt.xlabel("eta")
+    plt.ylabel("v")
+    #display the parameter values
+    plt.title("N = " + str(N) + ", V = " + str(V) + ", L = " + str(L) + ", R = " + str(R))
+    plt.show()
+
+    
+plot()
