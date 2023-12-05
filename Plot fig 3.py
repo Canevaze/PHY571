dc0b964785e4723a5470e07e39ff654777f249e4
Clément Brunel <126831841+brunelclement@users.noreply.github.com>
2023-12-05 15:07:48 +0100

    find straightest plot

--- Plot fig 3.py
+++ Plot fig 3.py
@@ -4,7 +4,9 @@ import itertools
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import FuncAnimation
-#from tqdm import tqdm
+from tqdm import tqdm
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 from Class import *
         
@@ -33,7 +35,7 @@ def plot_v_vs_eta_subplot(v, eta, eta_critical_values):
         # Plot v in function of (eta_critical - eta) / eta_critical in each subplot
         axes[i].scatter(x_values, v, label=f'eta_critical = {eta_critical}')
         
-        axes[i].set_ylim(0.1, 1)
+        axes[i].set_ylim(0.6, 1)
         axes[i].set_yscale('log')
         axes[i].set_xlim(0.01, 1)
         axes[i].set_xscale('log')
@@ -56,8 +58,37 @@ def plot_v_vs_eta_subplot(v, eta, eta_critical_values):
 
 # Example usage:
 # Generate some example data
-eta_critical_values = [4,4]
+eta_critical_values = [3,4]
 
 # Call the function to plot the data with different eta_critical values
 plot_v_vs_eta_subplot(v, eta, eta_critical_values)
+
+# i want to build a function that finds the parameter eta_critical for which the plot is the straightest
+
+def find_eta_critical():
+    eta_critical_values = np.linspace(0.1, 5, 100)
+    v = np.load('v_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
+    eta = np.load('eta_it_200_N_100_V_0.03_L_5_R_1_avg_20_nb_pts_50.npy')
+    
+    v_eta_critical = []
+    for eta_critical in eta_critical_values:
+        x_values = (eta_critical - eta) / eta_critical
+        v_eta_critical.append(np.polyfit(x_values, v, 1)[0])
+    return eta_critical_values[np.argmax(v_eta_critical)]
+
+def find_eta_critical_v2(eta_range, generate_plot_data):
+    min_error = float('inf')
+    best_eta = None
+
+    for eta in eta_range:
+        x, y = generate_plot_data(eta)
+        model = LinearRegression().fit(x.reshape(-1, 1), y)
+        y_pred = model.predict(x.reshape(-1, 1))
+        error = mean_squared_error(y, y_pred)
+
+        if error < min_error:
+            min_error = error
+            best_eta = eta
+
+    return best_eta
 
